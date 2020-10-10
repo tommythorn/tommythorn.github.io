1e17b861a791004a93d9de58df8300ebd72b8273
Tommy Thorn <tommy-github2@thorn.ws>
2020-10-09 23:36:20 -0700

    add life

---
+++ life/pkg/wasm_game_of_life.d.ts
@@ -0,0 +1,51 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export enum Cell {
+  Dead,
+  Alive,
+}
+/**
+*/
+export class Universe {
+  free(): void;
+/**
+*/
+  tick(): void;
+/**
+* @returns {Universe}
+*/
+  static new(): Universe;
+/**
+* @returns {number}
+*/
+  width(): number;
+/**
+* Set the width of the universe.
+*
+* Resets all cells to the dead state.
+* @param {number} width
+*/
+  set_width(width: number): void;
+/**
+* @returns {number}
+*/
+  height(): number;
+/**
+* Set the height of the universe.
+*
+* Resets all cells to the dead state.
+* @param {number} height
+*/
+  set_height(height: number): void;
+/**
+* @returns {number}
+*/
+  cells(): number;
+/**
+* @param {number} row
+* @param {number} column
+*/
+  toggle_cell(row: number, column: number): void;
+}
